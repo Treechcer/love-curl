1ed980d5502dca99f4772f66ab9cbb9d5e8bb343
Treechcer <treechcer@gmail.com>
2026-06-05 21:44:01 +0200

    added more functionality to this library, like parsing JSONs and unparsing them (haha)

--- lib-main.lua
+++ lib-main.lua
@@ -1,10 +1,27 @@
 --body = os.execute('curl https://jsonplaceholder.typicode.com/todos/1 -H "Accept: application/json"')
 http = {}
 
+---@param str string
+function http.JSONparser(str) --parses string JSON into Lua table
+    return str:gsub('"([^"]+)"%s*:%s*', '["%1"] = ')
+end
+
+function http.StringParser(jsonTable)
+    str = "{"
+    
+    for key, value in pairs(jsonTable) do
+        quotes = (type(value) == "string") and '"' or ""
+        str = str .. '"' .. key .. '" : ' .. quotes .. tostring(value) .. quotes .. ","
+    end
+
+    return str:sub(1, str:len() - 1) .. "}"
+end
+
 ---@param URL string
 ---@param curlVars string?
 ---@return string
 function http.reqest(URL, curlVars)
+    curlVars = curlVars or ""
     handler = io.popen('curl ' .. URL .. " " .. curlVars .. ' 2>/dev/null')
     body = handler:read("a")
     handler:close()
@@ -16,15 +33,10 @@ end
 ---@param curlVars string?
 ---@return table
 function http.JSONRequest(URL, curlVars) --auomatically add '-H "Accept: application/json!"' to your request for JSON and returns in as a lua table
-    handler = io.popen('curl ' .. URL .. " " .. curlVars .. ' -H "Accept: application/json!"' .. ' 2>/dev/null')
-    body = handler:read("a")
-    handler:close()
+    curlVars = curlVars or ""
+    body = loadstring("return " .. http.JSONparser(http.reqest(URL, curlVars .. ' -H "Accept: application/json!"')))()
 
-    body = body:gsub('"([^"]+)"%s*:%s*', '["%1"] = ')
-
-    a = loadstring("return " .. body)()
-
-    return a
+    return body
 end
 
 return http
--- main.lua
+++ main.lua
@@ -2,7 +2,8 @@ love = require("love")
 
 function love.load()
     http = require("lib-main")
-    req = http.reqest('https://jsonplaceholder.typicode.com/todos/1', '-H "Accept: application/json!"')
+    --req = http.reqest('https://jsonplaceholder.typicode.com/todos/1', '-H "Accept: application/json!"')
+    req = http.StringParser(http.JSONRequest('https://jsonplaceholder.typicode.com/todos/1'))
 
     print(req)
 end
