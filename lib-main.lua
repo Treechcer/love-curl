895cc15f8db8e715d3aff998a0de8e8f5d221c54
Treechcer <treechcer@gmail.com>
2026-06-05 13:34:56 +0200

    initial commit, made it so you can make curl http request in lua and get back table

--- lib-main.lua
+++ lib-main.lua
@@ -1,8 +1,22 @@
 --body = os.execute('curl https://jsonplaceholder.typicode.com/todos/1 -H "Accept: application/json"')
 http = {}
 
+---@param URL string
+---@param curlVars string?
+---@return string
 function http.reqest(URL, curlVars)
     handler = io.popen('curl ' .. URL .. " " .. curlVars .. ' 2>/dev/null')
+    body = handler:read("a")
+    handler:close()
+
+    return body
+end
+
+---@param URL string
+---@param curlVars string?
+---@return table
+function http.JSONRequest(URL, curlVars) --auomatically add '-H "Accept: application/json!"' to your request for JSON and returns in as a lua table
+    handler = io.popen('curl ' .. URL .. " " .. curlVars .. ' -H "Accept: application/json!"' .. ' 2>/dev/null')
     body = handler:read("a")
     handler:close()
 
